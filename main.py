b50ab69f3ee52c88f16da5511fb7785162022607
roshankumbharx <roshankumbhar9987@gmail.com>
2025-06-15 00:16:48 +0530

    Logging updated

---
+++ main.py
@@ -0,0 +1,3 @@
+from src.ml_project import logger
+
+logger.info('This is our custom log!')
--- template.py
+++ template.py
@@ -8,10 +8,10 @@ project_name='ml_project'
 
 
 list_of_files=[
-    f"src{project_name}/__init__.py",
-    f"src{project_name}/components/__init__.py",
-    f"src{project_name}/utils/__init__.py",
-    f"src{project_name}/utils/common.py",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
     f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
